eee725df2557a24153f5fc27cc7b7c8442c728d2
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-01-05 14:34:50 +0800

    modified logo text content and style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THE <span className='warlocks'>WARLOCKS</span> ARE COMING...
         </p>
         <a
           className="App-link"
