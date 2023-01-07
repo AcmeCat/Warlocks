f992d81a824f2547a8e97a4315cc35ca0204c609
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-01-07 19:51:37 +0800

    added race param

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       name: "NAME",
+      race: "Human",
       patron: "The Void",
       level: 1
     }
@@ -16,6 +17,7 @@ class App extends React.Component {
   letsBegin () {
     this.setState({
       name: this.getName(),
+      race: this.getRace(),
       patron: this.getPatron(),
       level: this.getLevel()
     })
@@ -41,6 +43,7 @@ class App extends React.Component {
         </header>
         <main>
           <h2>{this.state.name}</h2>
+          <p><span>Race: </span>{this.state.race}</p>
           <p><span>Patron: </span>{this.state.patron}</p>
           <p><span>Level: </span>{this.state.level}</p>
         </main>
@@ -48,6 +51,8 @@ class App extends React.Component {
       </div>
     );
   }
+
+  // warlock details
 
   getName = () => {
     let name = this.randomItem(this.firstNames);
@@ -59,11 +64,17 @@ class App extends React.Component {
     return name; 
   }
 
+  getRace = () => {
+    return this.randomItem(this.races);
+  }
+
   getPatron = () => {
     return this.randomItem(this.patrons);
   }
 
   getLevel = () => {return Math.floor(Math.random() * 20 + 1);}
+
+  //utility functions
 
   randomItem = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -75,15 +86,17 @@ class App extends React.Component {
 
   firstNames = ['Harry', 'Gerry', 'Mavis', 'Verdun', 'Greg', 'Tim', 'Bartelomeo', 'Pontius', 'Gront', 'Miff', 'Pildrum', 'Arriety', 'Norj', 'Sem'];
   nameAdjectives = ['Magnificent', 'Magic', 'Exceptional', 'Rugged', 'Wicked', 'Pestilent', 'Mad', 'Sickening', 'Malodorous', 'Rad', 'Malevolent', 'Bright', 'Dim', 'Gloomy', 'Pensive', 'Miserly']
-  patrons = ['The Entity', 'Entropy', 'Ancient Unicorn', 'Arch Fey', 'Madness', 'Elder God', 'Arch Fiend', 'Devil', 'The Depths', 'Darkness', 'Celestial', 'Arch Lich'];
-
+  patrons = ['The Entity', 'Entropy', 'Ancient Unicorn', 'Arch Fey', 'Madness', 'Elder God', 'Arch Fiend', 'Devil', 'The Depths', 'Darkness', 'Celestial', 'Arch Lich', 'The Void'];
+  races = ['Human', 'Elf', 'Half-Elf', 'Dwarf', 'Halfling', 'Gnome', 'Dragonborn', 'Half-Orc', 'Tiefling'];
 }
 
 function Footer() {
   return (
     <div className='Footer'>
+      <hr/>
       <p>Wizards of the Coast, Dungeons & Dragons, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 2015 Wizards. All Rights Reserved.</p>
       <p>This (Web site) is not affiliated with, endorsed, sponsored, or specifically approved by Wizards of the Coast LLC. This (Web site) may use the trademarks and other intellectual property of Wizards of the Coast LLC, which is permitted under Wizards' Fan Site Policy (link). For example, Dungeons & Dragons® is a trademark(s) of Wizards of the Coast. For more information about Wizards of the Coast or any of Wizards' trademarks or other intellectual property, please visit their website at (www.wizards.com).</p>
+      <hr/>
     </div>
   )
 }
