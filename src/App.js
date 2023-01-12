51289d8f3eb480150b93a94bbebb3280391686d2
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-01-12 21:13:29 +0800

    generated spell list for each warlock.

--- src/App.js
+++ src/App.js
@@ -99,9 +99,7 @@ class App extends React.Component {
           <p><span className='title'>Proficiency Bonus: </span> +{this.state.proficiency}</p>
           <p><span className='title'>Saving Throws: </span> WIS +{this.state.proficiency + this.getModFromStat(this.state.WIS)}, CHA +{this.state.proficiency + this.getModFromStat(this.state.CHA)}</p>
           <hr/>
-          <Spells
-            level={this.state.level}
-          />
+          <Spells level={this.state.level}/>
         </main>
         <Footer/>
       </div>
@@ -223,16 +221,59 @@ class App extends React.Component {
 }
 
 function Spells({level}) {
-
   let numCantrips = Number.parseInt(data.levels[level]["cantrips"]);
   let allCantrips = [...data.spells.cantrips];
   let selectedCantrips = ["Eldritch Blast"];
   for (let i = 1; i < numCantrips; i++) {
     let ran = Math.floor(Math.random() * allCantrips.length)
-    selectedCantrips.push(allCantrips.splice(ran, 1));
+    selectedCantrips.push(allCantrips.splice(ran, 1)[0]);
+  }
+  let leveledSpells = [[], [], [], [], [], [], [], [], []]
+  let spellsPerLevel = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let slotLevel = data.levels[level]["slot level"];
+  let numberOfSpells = data.levels[level]["spells known"];
+
+  for (let i = 0; i < slotLevel; i++) {
+    spellsPerLevel[i] = 2;
+    numberOfSpells -= 2;
+  }
+  for (let i = numberOfSpells; i > 0; i--) {
+    let spellLevel = Math.floor(Math.random() * data.levels[level]["slot level"])
+    spellsPerLevel[spellLevel] += 1
   }
 
-  let selectedSpells = [selectedCantrips, ["Armor of Agathys"], [], [], [], [], [], [], [], []];
+  if (level > 10) {
+    spellsPerLevel[5] = 1;
+    if (level > 12) {
+      spellsPerLevel[6] = 1;
+      if (level > 14) {
+        spellsPerLevel[7] = 1;
+        if (level > 16) {
+          spellsPerLevel[8] = 1;
+        }
+      }
+    }
+  }
+
+  leveledSpells = spellsPerLevel.map((ele, ind) => {
+    let arr = [];
+    let lvl = (ind + 1).toString();
+    let spellList = [...data.spells[lvl]];
+    for (let i = 0; i < ele; i++) {
+      let ran = Math.floor(Math.random() * spellList.length);
+      let spell = spellList.splice(ran, 1)[0];
+      arr.push(spell);
+    }
+    return arr;
+  });
+
+  let selectedSpells = [selectedCantrips, ...leveledSpells];
+
+  let getSpells = (lvl) => {
+    if (selectedSpells[lvl].length !== 0) {
+      return <p><span className='title'>Level {lvl}{lvl > 5 ? "(Arcanum)" : ""}: </span> {selectedSpells[lvl].join(", ")}</p>
+    }
+  }
 
   return (
     <div>
@@ -240,16 +281,20 @@ function Spells({level}) {
       <p><span className='title'>Spell Slots: </span> {data.levels[level]["spell slots"]} <span className='title'>Slot Level:</span> {data.levels[level]["slot level"]}</p>
       <p><span className='title'>Cantrips: </span> {selectedSpells[0].join(", ")}</p>
       <p><span className='title'>Level 1: </span> {selectedSpells[1].join(", ")}</p>
+      {getSpells(2)}
+      {getSpells(3)}
+      {getSpells(4)}
+      {getSpells(5)}
+      {getSpells(6)}
+      {getSpells(7)}
+      {getSpells(8)}
+      {getSpells(9)}
     </div> 
   )
-  //spells
-
-  
-  
-  
 }
 
 function Footer() {
+  //console.log("calling footer")
   return (
     <div className='Footer'>
       <hr/>
