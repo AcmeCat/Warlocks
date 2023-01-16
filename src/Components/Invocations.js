6cdae24b69ad850032b64e6c30b67cfd4b62a633
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-01-16 22:47:05 +0800

    invocation list generated for level and pact prereqs

--- src/Components/Invocations.js
+++ src/Components/Invocations.js
@@ -3,14 +3,26 @@ import data from '../data.json';
 
 function Invocations({level, pact}) {
 
-    let invocations = []
-    let number = data.levels[level]["invocations"]
+    let invocations = [];
+    let available = [];
+    let numberOfInv = data.levels[level]["invocations"];
+    for (const prereqLevel in data.invocations) {
+        if (prereqLevel <= level && data.invocations[prereqLevel].hasOwnProperty("All")) {
+            available.push(...data.invocations[prereqLevel]["All"])
+            if (data.invocations[prereqLevel].hasOwnProperty(pact)) {
+                available.push(...data.invocations[prereqLevel][pact])
+            }
+        }
+    }
+
+    for (let i = 0; i < numberOfInv; i++) {
+        let ran = Math.floor(Math.random() * available.length);
+        invocations.push(available.splice(ran, 1));
+    }
 
     return (
         <div>
-          <h3>Invovations</h3>
-          <p>number: {number}</p>
-          <p>pact: {pact}</p>
+          <h3>Invocations</h3>
           <p>{invocations.join(", ")}</p>
         </div> 
     )
