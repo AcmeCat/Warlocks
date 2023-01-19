31a9f142b0389f51521cd8a3f96b6059b9ce83b8
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-01-19 12:29:45 +0800

    bug fix : attribute containers compress too much on small screens

--- src/Components/Attributes.js
+++ src/Components/Attributes.js
@@ -6,36 +6,36 @@ function Attributes({att}) {
         <div>
             <div className='attribute-flex'>
                 <div className='attribute'>
-                    <h4>STR</h4>
-                    <p className='value'>{att.STR.value}</p>
-                    <p>({modString(att.STR.mod)})</p>
+                    <div className='attribute-name'>STR</div>
+                    <div className='value'>{att.STR.value}</div>
+                    <div className='modifier'>({modString(att.STR.mod)})</div>
                 </div>
                 <div className='attribute'>
-                    <h4>DEX</h4>
-                    <p className='value'>{att.DEX.value}</p>
-                    <p>({modString(att.DEX.mod)})</p>
+                    <div className='attribute-name'>DEX</div>
+                    <div className='value'>{att.DEX.value}</div>
+                    <div className='modifier'>({modString(att.DEX.mod)})</div>
                 </div>
                 <div className='attribute'>
-                    <h4>CON</h4>
-                    <p className='value'>{att.CON.value}</p>
-                    <p>({modString(att.CON.mod)})</p>
+                    <div className='attribute-name'>CON</div>
+                    <div className='value'>{att.CON.value}</div>
+                    <div className='modifier'>({modString(att.CON.mod)})</div>
                 </div>
             </div>
             <div className='attribute-flex'>
                 <div className='attribute'>
-                    <h4>INT</h4>
-                    <p className='value'>{att.INT.value}</p>
-                    <p>({modString(att.INT.mod)})</p>
+                    <div className='attribute-name'>INT</div>
+                    <div className='value'>{att.INT.value}</div>
+                    <div className='modifier'>({modString(att.INT.mod)})</div>
                 </div>
                 <div className='attribute'>
-                    <h4>WIS</h4>
-                    <p className='value'>{att.WIS.value}</p>
-                    <p>({modString(att.WIS.mod)})</p>
+                    <div className='attribute-name'>WIS</div>
+                    <div className='value'>{att.WIS.value}</div>
+                    <div className='modifier'>({modString(att.WIS.mod)})</div>
                 </div>
                 <div className='attribute'>
-                    <h4>CHA</h4>
-                    <p className='value'>{att.CHA.value}</p>
-                    <p>({modString(att.CHA.mod)})</p>
+                    <div className='attribute-name'>CHA</div>
+                    <div className='value'>{att.CHA.value}</div>
+                    <div className='modifier'>({modString(att.CHA.mod)})</div>
                 </div>
             </div>
         </div>
