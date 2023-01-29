2c0659f999ea82d0d39df67463d3daba10c0c9c5
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-01-29 14:06:32 +0800

    bugfix: randomBool not working in NameAndTitle

--- src/Components/NameAndTitle.js
+++ src/Components/NameAndTitle.js
@@ -3,11 +3,16 @@ import data from '../Data/patrons.json';
 
 function NameAndTitle({details}) {
     let patrons = data[details.patron];
+
+    function randomBool (number) {
+        return Math.floor(Math.random * 100) < number;
+    }
+
     return (
       <div>
         <h2 className='warlock-name'>{details.name}</h2>
         <p className='running-title'>{details.species} Warlock, 
-            {this.randomBool(33) ? 'Devotee' : this.randomBool(50) ? 'Servant' : 'Minion'} of {details.patron}
+            {randomBool(33) ? 'Devotee' : randomBool(50) ? 'Servant' : 'Minion'} of {details.patron}
             {details.pact !== '' ? ', Pact of the ' + details.pact : ''}</p>
       </div> 
     )
